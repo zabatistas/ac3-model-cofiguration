d9077d70b495a537b26d03f847b7b792f733310c
zabatistas <zabatistas@gmail.com>
2025-09-22 14:46:42 +0300

    First commit

---
+++ exampleconfig/src/main/java/com/uprc/exampleconfig/ExampleconfigApplication.java
@@ -0,0 +1,13 @@
+package com.uprc.exampleconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleconfigApplication.class, args);
+	}
+
+}
---
+++ exampleconfig/src/test/java/com/uprc/exampleconfig/ExampleconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uprc.exampleconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
